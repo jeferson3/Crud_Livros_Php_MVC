84d2a9b6572f6d1feecccef344fe67df86f3933a
jeferson <jefersongomes333@hotmail.com>
2020-07-01 16:26:54 -0300

    Update update.php

--- update.php
+++ update.php
@@ -3,13 +3,13 @@
 if ($_POST['atualizar']) {
     require_once('conexao.php');
 
-    $id = isset($_POST['id']) ? $_POST['id'] : 'id';
+    $id = isset($_GET['id']) ? $_GET['id'] : 'id';
     $nome = isset($_POST['nome']) ? $_POST['nome'] : 'nome';
     $sobrenome = isset($_POST['sobrenome']) ? $_POST['sobrenome'] : 'sobrenome';
     $email = isset($_POST['email']) ? $_POST['email'] : 'email';
     $descricao = isset($_POST['txt']) ? $_POST['txt'] : 'descricao';
 
-    session_start();
+    // session_start();
 
     header('location: index.php');
 }
