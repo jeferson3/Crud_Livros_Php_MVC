c883d2772d5141e2a35b944c1c1cedd65070694a
jeferson <jefersongomes333@hotmail.com>
2020-07-01 13:33:34 -0300

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -7,14 +7,13 @@ if ($_POST['adicionar']) {
     $sobrenome = isset($_POST['sobrenome']) ? $_POST['sobrenome'] : 'sobrenome';
     $email = isset($_POST['email']) ? $_POST['email'] : 'email';
     $descricao = isset($_POST['txt']) ? $_POST['txt'] : 'descricao';
-    echo $nome;
 
-    // $crud = new Crud("localhost", "crud", "root", "");
-    // $con = $crud->conexao();
-
-    // $crud->inserirDados($con, [$nome, $sobrenome, $email, $descricao]);
     session_start();
-    $_SESSION['message'] = 'Criado com sucesso';
-    $_SESSION['messageType'] = 'success';
+
+    $crud = new Crud("localhost", "crud", "root", "");
+    $con = $crud->conexao();
+    $crud->inserirDados($con, [$nome, $sobrenome, $email, $descricao]);
+
+
     header("location: index.php");
 }
