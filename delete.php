4adf533a85dd4db74dec3f2f98a4c69ddaaeb2e8
jeferson <jefersongomes333@hotmail.com>
2020-07-01 13:33:40 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,14 +1,14 @@
 <?php
 
 
-if($_GET['id']){
-    
+if ($_GET['id']) {
+
     require_once('conexao.php');
     
+    session_start();
     $crud = new Crud('localhost', 'crud', 'root', '');
     $con = $crud->conexao();
     $crud->deletarDados($con, $_GET['id']);
-    
 }
 
 header("location: index.php");
