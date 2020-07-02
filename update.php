5b010526a625e4b2cd8b9ca7e0b26598ac3c60dc
jeferson <jefersongomes333@hotmail.com>
2020-07-01 21:00:27 -0300

    Update update.php

--- update.php
+++ update.php
@@ -3,13 +3,18 @@
 if ($_POST['atualizar']) {
     require_once('conexao.php');
 
-    $id = isset($_GET['id']) ? $_GET['id'] : 'id';
+    $id = isset($_POST['id']) ? $_POST['id'] : 'id';
     $nome = isset($_POST['nome']) ? $_POST['nome'] : 'nome';
     $sobrenome = isset($_POST['sobrenome']) ? $_POST['sobrenome'] : 'sobrenome';
     $email = isset($_POST['email']) ? $_POST['email'] : 'email';
     $descricao = isset($_POST['txt']) ? $_POST['txt'] : 'descricao';
 
-    // session_start();
+    echo $id, $nome, $sobrenome;
 
+    session_start();
+
+    $crud = new Crud("localhost", "crud", "root", "");
+    $con = $crud->conexao();
+    $crud->atualizarDados($con, $id, [$nome, $sobrenome, $email, $descricao]);
     header('location: index.php');
 }
